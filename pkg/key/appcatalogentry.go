e8790c62b09664bae82afff3bc45bc368373f60d
Ross Fairbanks <ross@rossfairbanks.com>
2021-02-18 16:16:54 +0100

    Add key.AppCatalogEntryTeam function

--- pkg/key/appcatalogentry.go
+++ pkg/key/appcatalogentry.go
@@ -2,6 +2,10 @@ package key
 
 import (
 	"fmt"
+
+	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
+
+	"github.com/giantswarm/app/v4/pkg/annotation"
 )
 
 func AppCatalogEntryManagedBy(projectName string) string {
@@ -10,4 +14,8 @@ func AppCatalogEntryManagedBy(projectName string) string {
 
 func AppCatalogEntryName(catalogName, appName, appVersion string) string {
 	return fmt.Sprintf("%s-%s-%s", catalogName, appName, appVersion)
+}
+
+func AppCatalogEntryTeam(customResource v1alpha1.App) string {
+	return customResource.Annotations[annotation.Team]
 }
