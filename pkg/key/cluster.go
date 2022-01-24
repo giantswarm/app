bbee3fb8e5ba5cbf3e99de5fa4cec681a8d6127e
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2022-01-24 18:40:02 +0100

    Adjusting functions for org-namespaced apps

--- pkg/key/cluster.go
+++ pkg/key/cluster.go
@@ -17,9 +17,17 @@ func ClusterConfigMapName(customResource v1alpha1.App) string {
 		return ingressControllerConfigMapName
 	}
 
+	if IsInOrgNamespace(customResource) {
+		return fmt.Sprintf("%s-cluster-values", ClusterLabel(customResource))
+	}
+
 	return fmt.Sprintf("%s-cluster-values", customResource.Namespace)
 }
 
 func ClusterKubeConfigSecretName(customResource v1alpha1.App) string {
+	if IsInOrgNamespace(customResource) {
+		return fmt.Sprintf("%s-kubeconfig", ClusterLabel(customResource))
+	}
+
 	return fmt.Sprintf("%s-kubeconfig", customResource.Namespace)
 }
